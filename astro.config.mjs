fbaa9c15739132713eb8dc74db283a142cdb1b6b
Elio <eliomanuelleonlopez@gmail.com>
2026-04-03 01:35:03 +0200

    Se implementa tailwind correctaemente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://elioleon.github.io',
     base: '/asturias-vivienda/', 
     trailingSlash: 'always',
+    vite: {
+    plugins: [tailwindcss()],
+  },
 });
